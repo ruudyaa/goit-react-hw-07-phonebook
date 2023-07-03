ce0ad97f80c525b85c8e77e35b6180a6e6ef635f
ruudyaa <martarudnytska26@gmail.com>
2023-07-03 03:16:19 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,17 +1,25 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const getContacts = store => store.contacts;
+//export const selectContacts = ({ contacts }) => contacts.items;
+export const selectIsLoading = ({ contacts }) => contacts.isLoading;
+export const selectError = ({ contacts }) => contacts.error;
+export const selectFilter = ({ filter }) => filter;
 
-export const getFilter = store => store.filter;
+export const selectContacts = ({ contacts }) =>
+  [...contacts.items].sort((a, b) => a.name.localeCompare(b.name));
 
-export const getFilteredContacts = createSelector(
-  [getContacts, getFilter],
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
   (contacts, filter) => {
+    if (!filter) {
+      return contacts;
+    }
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(
-      ({ name, number }) =>
+    const filteredContacts = contacts.filter(
+      ({ name, phone }) =>
         name.toLowerCase().trim().includes(normalizedFilter) ||
-        number.trim().includes(normalizedFilter)
+        phone.trim().includes(normalizedFilter)
     );
+    return filteredContacts;
   }
 );
